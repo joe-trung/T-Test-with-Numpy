d5cb6eafa09992afea2f157df4f5848ea1aea79d
trand <kieutrungtran@gmail.com>
2023-04-07 20:47:00 -0400

    done

--- ttest.py
+++ ttest.py
@@ -3,3 +3,18 @@ import numpy as np
 from scipy import stats
 import os
 
+df = pd.read_csv('scores.csv')
+print(df.shape)
+print(df)
+
+fm = df.loc[df['gender'] == 'female', 'math score'].values
+print(type(fm))
+print(fm)
+
+mm = df.loc[df['gender'] == 'male', 'math score'].values
+print(type(mm))
+print(mm)
+
+print(mm.size,' , ', fm.size)
+
+
